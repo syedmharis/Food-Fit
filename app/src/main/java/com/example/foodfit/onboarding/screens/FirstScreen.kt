b7b1dcfa96877184903517fee1b29a82d37a5fcc
Syed Muhammad Haris <syedmuhammadharis@pk.folio3.com>
2023-11-16 19:06:22 +0500

    Initial code with Splash & Landing Screens Setup.

---
+++ app/src/main/java/com/example/foodfit/onboarding/screens/FirstScreen.kt
@@ -0,0 +1,37 @@
+package com.example.foodfit.onboarding.screens
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
+import com.example.foodfit.R
+import com.example.foodfit.databinding.FragmentFirstScreenBinding
+
+class FirstScreen : Fragment() {
+
+    private var _binding: FragmentFirstScreenBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentFirstScreenBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        val viewPager = activity?.findViewById<ViewPager2>(R.id.viewPager)
+
+        binding.next.setOnClickListener {
+            viewPager?.currentItem = 1
+        }
+
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/foodfit/onboarding/screens/ThirdScreen.kt
@@ -0,0 +1,44 @@
+package com.example.foodfit.onboarding.screens
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.foodfit.R
+import com.example.foodfit.databinding.FragmentThirdScreenBinding
+
+class ThirdScreen : Fragment() {
+
+    private var _binding: FragmentThirdScreenBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentThirdScreenBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        binding.finish.setOnClickListener {
+            findNavController().navigate(R.id.action_viewPagerFragment_to_homeFragment)
+            onBoardingFinished()
+        }
+
+        return view
+    }
+
+    private fun onBoardingFinished() {
+        val sharedPref = requireActivity().getSharedPreferences("onBoarding", Context.MODE_PRIVATE)
+        val editor = sharedPref.edit()
+        editor.putBoolean("Finished", true)
+        editor.apply()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
